a69e8c6a6123b88ec84bac99a173b4416fe7347c
XavierZambrano <xavierzamco@gmail.com>
2024-06-25 23:59:30 -0600

    feat: parse

--- fide_scraper/spiders/top_players.py
+++ fide_scraper/spiders/top_players.py
@@ -4,7 +4,16 @@ import scrapy
 class TopPlayersSpider(scrapy.Spider):
     name = "top_players"
     allowed_domains = ["ratings.fide.com"]
-    start_urls = ["https://ratings.fide.com"]
+    start_urls = ["https://ratings.fide.com/a_top.phtml"]
 
     def parse(self, response):
-        pass
+        rows = response.xpath('//div[@id="top_rating_div"]//tr')
+        rows_body = rows[1:]
+        for container in rows_body:
+            yield {
+                "rank": container.xpath('.//td[1]/text()').get(),
+                "name": container.xpath('.//td[2]/a/text()').get(),
+                "federation": container.xpath('.//td[3]/text()[2]').get().strip(),
+                "rating": container.xpath('.//td[4]/text()').get(),
+                "born_year": container.xpath('.//td[6]/text()').get(),
+            }
